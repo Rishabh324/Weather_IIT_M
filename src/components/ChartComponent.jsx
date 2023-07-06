5ad6ed2d2a0342764ec7fdcf3d47ca215742b075
Rishabh Sinha <sinharishabh402@gmail.com>
2023-07-06 15:24:00 +0530

    Commit

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -1,30 +1,84 @@
 import React from 'react';
-import CanvasJSReact from '@canvasjs/react-charts';
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
 
 const ChartComponent = () => {
-    const options = {
-        height: 150,
-        width: 810,
-        axisY: {
-            interval: 0,
-            title: 'Degree'
+    const data = [
+        {
+            name: "0%",
+            uv: 28,
+            amt: 2400
         },
-        data: [{
-            type: "area",
-            dataPoints: [
-                { label: "20%", y: 28 },
-                { label: "40%", y: 30 },
-                { label: "60%", y: 29 },
-                { label: "80%", y: 29 },
-                { label: "100%", y: 27 }
-            ]
-        }]
-    }
+        {
+            name: "10%",
+            uv: 25,
+            amt: 2210
+        },
+        {
+            name: "20%",
+            uv: 29,
+            amt: 2290
+        },
+        {
+            name: "30%",
+            uv: 26,
+            amt: 2000
+        },
+        {
+            name: "40%",
+            uv: 27,
+            amt: 2181
+        },
+        {
+            name: "50%",
+            uv: 28,
+            amt: 2500
+        },
+        {
+            name: "60%",
+            uv: 24,
+            amt: 2100
+        },
+        {
+            name: "70%",
+            uv: 30,
+            amt: 2100
+        },
+        {
+            name: "80%",
+            uv: 27,
+            amt: 2100
+        },
+        {
+            name: "90%",
+            uv: 32,
+            amt: 2100
+        },
+        {
+            name: "100%",
+            uv: 26,
+            amt: 2100
+        }
+    ];
 
     return (
         <div className='chart-div' style={{ height: '150px' }}>
-            <CanvasJSChart options={options} />
+            <AreaChart
+                width={870}
+                height={150}
+                data={data}
+                margin={{
+                    top: 10,
+                    right: 30,
+                    left: 0,
+                    bottom: 0
+                }}
+            >
+                <CartesianGrid strokeDasharray="110 1" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Area type="monotone" dataKey="uv" stroke="#5C9CE5" fill="#5C9CE5" />
+            </AreaChart>
         </div>
     );
 }
